0dd9be172f810f7fc66d0fba24f284c1659abe38
ymyang <ymyang@163.com>
2017-04-08 22:59:43 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const bannerPlugin = new webpack.BannerPlugin({
+  banner: '// { "framework": "Vue" }\n',
+  raw: true
+})
+
+function getBaseConfig () {
+  return {
+    entry: {
+      'index': path.resolve('src', 'app.js')
+    },
+    output: {
+      path: 'dist',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.vue(\?[^?]+)?$/,
+          loaders: []
+        }
+      ]
+    },
+    plugins: [
+      // new webpack.optimize.UglifyJsPlugin({compress: { warnings: false }}),
+      bannerPlugin
+    ]
+  }
+}
+
+const webConfig = getBaseConfig()
+webConfig.output.filename = '[name].web.js'
+webConfig.module.rules[1].loaders.push('vue-loader')
+
+const nativeConfig = getBaseConfig()
+nativeConfig.output.filename = '[name].weex.js'
+nativeConfig.module.rules[1].loaders.push('weex-loader')
+
+module.exports = [webConfig, nativeConfig]
